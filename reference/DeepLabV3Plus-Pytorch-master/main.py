300048c476a0b9d22d7d27c6dec659d13070fc3b
HY Tang <wf1497b@gmail.com>
2022-06-16 23:52:21 +0800

    Update main.py

--- reference/DeepLabV3Plus-Pytorch-master/main.py
+++ reference/DeepLabV3Plus-Pytorch-master/main.py
@@ -307,7 +307,6 @@ def main():
     else:
         print("[!] Retrain")
         model = nn.DataParallel(model)
-        print("Model to Device")
         model.to(device)
 
     # ==========   Train Loop   ==========#
@@ -323,6 +322,7 @@ def main():
         return
 
     interval_loss = 0
+    
     print("=====  Training Starts =====")
     while True:  # cur_itrs < opts.total_itrs:
         # =====  Train  =====
