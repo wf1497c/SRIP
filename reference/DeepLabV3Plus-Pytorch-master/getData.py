fe18bb90e188e144434916734846db83db977771
wf <wf1497b@gmail.com>
2022-06-17 20:45:19 +0800

    getData upload

--- reference/DeepLabV3Plus-Pytorch-master/getData.py
+++ reference/DeepLabV3Plus-Pytorch-master/getData.py
@@ -5,3 +5,6 @@ with zipfile.ZipFile("leftImg8bit_trainvaltest.zip","r") as zip_ref:
 
 with zipfile.ZipFile("gtFine_trainvaltest.zip","r") as zip_ref:
     zip_ref.extractall("./datasets/data/cityscapes/")
+
+with zipfile.ZipFile("leftImg8bit_blurred.zip","r") as zip_ref:
+    zip_ref.extractall("./test_images/cityscapes/")
