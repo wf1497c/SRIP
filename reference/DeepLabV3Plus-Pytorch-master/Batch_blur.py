3d2d09d6a0a8fceb03240b82e666e9d5e87abc7d
wf <wf1497b@gmail.com>
2022-06-23 14:41:07 +0800

    upload

--- reference/DeepLabV3Plus-Pytorch-master/Batch_blur.py
+++ reference/DeepLabV3Plus-Pytorch-master/Batch_blur.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import cv2
 import os
 
-root = "./datasets/cityscapes/leftImg8bit/train"
+root = "./datasets/data/cityscapes/leftImg8bit"
 num = os.listdir(root)
-for dirname in os.listdir(root):
-    path = os.path.join(root,dirname)
-    for filename in os.listdir(path):
-        file = os.path.join(path,filename)
-        img = plt.imread(file)
-        img = cv2.GaussianBlur(img,(31,31),15,15)
-        plt.imsave(file,img)
+for dir in os.listdir(root):
+    path1 = os.path.join(root,dir)
+    for dirname in os.listdir(path1):
+        path = os.path.join(path1,dirname)
+        for filename in os.listdir(path):
+            file = os.path.join(path,filename)
+            img = plt.imread(file)
+            img = cv2.GaussianBlur(img,(31,31),15,15)
+            plt.imsave(file,img)
